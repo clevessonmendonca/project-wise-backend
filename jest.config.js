e07ced6a886c6ae0a4976f598d73381822211318
clevessonmendonca <wanderbloks311@gmail.com>
2024-09-10 19:53:17 -0300

    chore: add configs

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 /** @type {import('jest').Config} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'js'],
-    rootDir: 'src',
-    testMatch: ['**/?(*.)+(spec|test).ts'],
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  rootDir: 'src',
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+};
